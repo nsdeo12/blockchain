3e4b1230292741457c7cc2758a05431a4618d020
nsdeo12 <nsdeo12@hotmail.com>
2018-08-23 23:12:52 +0530

    initial commit

---
+++ fabcar/app.js
@@ -0,0 +1,141 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('static-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var routes = require('./routes/index');
+var users = require('./routes/users');
+var query=require('./query');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(favicon());
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', function(){
+
+});
+app.use('/users', users);
+
+////////////////////////////////////////////
+
+var Fabric_Client = require('fabric-client');
+var path = require('path');
+var util = require('util');
+var os = require('os');
+
+//
+var fabric_client = new Fabric_Client();
+
+// setup the fabric network
+var channel = fabric_client.newChannel('mychannel');
+var peer = fabric_client.newPeer('grpc://localhost:7051');
+channel.addPeer(peer);
+
+//
+var member_user = null;
+var store_path = path.join(__dirname, 'hfc-key-store');
+console.log('Store path:'+store_path);
+var tx_id = null;
+
+// create the key value store as defined in the fabric-client/config/default.json 'key-value-store' setting
+Fabric_Client.newDefaultKeyValueStore({ path: store_path
+}).then((state_store) => {
+	// assign the store to the fabric client
+	fabric_client.setStateStore(state_store);
+	var crypto_suite = Fabric_Client.newCryptoSuite();
+	// use the same location for the state store (where the users' certificate are kept)
+	// and the crypto store (where the users' keys are kept)
+	var crypto_store = Fabric_Client.newCryptoKeyStore({path: store_path});
+	crypto_suite.setCryptoKeyStore(crypto_store);
+	fabric_client.setCryptoSuite(crypto_suite);
+
+	// get the enrolled user from persistence, this user will sign all requests
+	return fabric_client.getUserContext('user1', true);
+}).then((user_from_store) => {
+	if (user_from_store && user_from_store.isEnrolled()) {
+		console.log('Successfully loaded user1 from persistence');
+		member_user = user_from_store;
+	} else {
+		throw new Error('Failed to get user1.... run registerUser.js');
+	}
+
+	// queryCar chaincode function - requires 1 argument, ex: args: ['CAR4'],
+	// queryAllCars chaincode function - requires no arguments , ex: args: [''],
+	// const request = {
+	// 	//targets : --- letting this default to the peers assigned to the channel
+	// 	chaincodeId: 'fabcar',
+	// 	fcn: 'queryAllCars',
+	// 	args: ['']
+	// };
+	const request = {
+		//targets : --- letting this default to the peers assigned to the channel
+		chaincodeId: 'fabcar',
+		fcn: 'queryCar',
+		args: ['CAR10']
+	};
+	// send the query proposal to the peer
+	return channel.queryByChaincode(request);
+}).then((query_responses) => {
+	console.log("Query has completed, checking results");
+	// query_responses could have more than one  results if there multiple peers were used as targets
+	if (query_responses && query_responses.length == 1) {
+		if (query_responses[0] instanceof Error) {
+			console.error("error from query = ", query_responses[0]);
+		} else {
+			console.log("Response is ", query_responses[0].toString());
+		}
+	} else {
+		console.log("No payloads were returned from query");
+	}
+}).catch((err) => {
+	console.error('Failed to query successfully :: ' + err);
+});
+
+////////////////////////////////////////////
+
+
+
+/// catch 404 and forwarding to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+/// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+
+
+module.exports = app;
